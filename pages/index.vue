c7f1a059307c2d48f8c7c7dccd1ef8c05fcb5856
Alexandr Varygin <otz-job@mail.ru>
2020-04-26 21:31:50 +0300

    add bootstrap

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,12 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-theory
-      </h1>
-      <h2 class="subtitle">
-        I study nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section>
+    <h1>This is home page!</h1>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
