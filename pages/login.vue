192419065c5ce3b571144d40c5d1f29efea8d9a0
Alexandr Varygin <otz-job@mail.ru>
2020-04-28 22:00:43 +0300

    how use layouts, create empty.vue - without navbar and add this layouts to login.vue

---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+      <p>
+        <nuxt-link to="/">Home</nuxt-link>
+      </p>
+
+    <form>
+      <h1>Login</h1>
+
+      <div class="form-group">
+        <input type="text" class="form-control">
+      </div>
+
+      <div class="form-group">
+        <input type="password" class="form-control">
+      </div>
+
+      <button class="btn btn-primary">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+    export default {
+      layout: 'empty'
+      // layout: 'default' - значение по-умолчанию
+    }
+</script>
+
+<style scoped>
+  form {
+    width: 600px;
+  }
+</style>
