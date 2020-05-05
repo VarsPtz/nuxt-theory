5cb7d4fcde70763f69c347cf6f6f800717bc8763
Alexandr Varygin <otz-job@mail.ru>
2020-05-05 14:40:35 +0300

    use store Vuex - get userID with action fetchUserById()

--- store/users.js
+++ store/users.js
@@ -6,5 +6,12 @@ export const actions = {
     } catch (e) {
       throw e
     }
+  },
+  async fetchUserById({}, userId) {
+    try {
+      return await this.$axios.$get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    } catch (e) {
+      throw e
+    }
   }
 }
